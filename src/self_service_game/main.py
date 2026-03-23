56082c9fbfa279aa65e27b2d9e5c6973827eb67f
pali <prclash13@protonmail.com>
2026-03-23 03:24:11 -0300

    Código fonte adicionado

---
+++ src/self_service_game/main.py
@@ -0,0 +1,48 @@
+def main():	
+
+	menu = ['arroz', 'tutu', 'carne', 'batata frita', 'salada,', 'farofa']
+
+	print(menu)
+
+
+
+	prato_feito = []
+
+
+
+	while True:
+
+
+    	escolha = input('Escolha um alimento sem repetições ou pressione "Enter" para sair: ').strip().lower()
+
+
+    
+
+    	if escolha == "":
+
+        	break
+
+
+    	if escolha in menu:
+
+        	if escolha in prato_feito:
+        
+            	print('Alimento já selecionado')
+    
+
+        	else:
+
+            	prato_feito.append(escolha)
+
+            	print('Adicionado com sucesso')
+
+
+    	else:
+
+        	print('Alimento não disponível')
+
+
+	print(f'Seu prato feito: {prato_feito}')
+
+if __name__ == "__main__":
+    main()
